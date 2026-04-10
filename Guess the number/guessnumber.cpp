1e0b5fcdd7b48e50e469f575a1409672baa5742a
Cosminnnnn6 <muscacosminionut@gmail.com>
2026-04-10 12:55:57 +0200

    tttt

---
+++ Guess the number/guessnumber.cpp
@@ -0,0 +1,40 @@
+#include <iostream> 
+#include <cstdlib>
+
+using namespace std;
+
+int main(){
+    srand(time(0));
+
+//generate random number 1 - 100
+
+int randomNum,attempt,attemptUser;
+attemptUser=0;
+randomNum = rand() % 100+1;
+
+
+// make an attempt & save it
+do {
+        cout<<" Number= ";
+        cin>>attempt;
+        attemptUser++;
+        int difference = abs(randomNum - attempt);
+
+        if(attempt<randomNum) {
+
+            cout<<"Too low"<<endl;
+        }
+else if(attempt>randomNum) {
+    cout<<"Too high "<<endl;
+
+} else {
+    cout<< "Congrats"<<endl;
+    break;
+}
+
+
+} while(attempt != randomNum);
+
+return 0;
+
+}
